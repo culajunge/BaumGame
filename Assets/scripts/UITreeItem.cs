a57ae9976ef610b71281e8fa02ea51eb12d88e25
Actually_Rico <ricogajewski27@gmail.com>
2024-11-03 18:18:22 +0100

    init commit

---
+++ Assets/scripts/UITreeItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UITreeItem : MonoBehaviour
+{
+    [SerializeField] private Image preview;
+    [SerializeField] public TMP_Text amount;
+    [HideInInspector] public Sprite previewImg;
+    [HideInInspector] public int id;
+
+    [SerializeField] private Button button;
+
+    public void AddOnClickListener(UnityEngine.Events.UnityAction action)
+    {
+        button.onClick.AddListener(action);
+    }
+    
+    public void OnInstance()
+    {
+        preview.sprite = previewImg;
+    }
+}
